5549722957d5053e3db28cb350fa8f549a48eea5
xuanhu001 <61825756+xuanhu001@users.noreply.github.com>
2024-08-09 15:42:11 +0800

    create shell for show engineering

---
+++ sample/mv_files.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# mkdir /mnt/sophonWP/sophon-demo/sample/tmp
+# mv /mnt/sophonWP/sophon-demo/sample/P2PNet/P2PNet /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/PP-OCR/PP-OCR /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/Real-ESRGAN/Real-ESRGAN /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/SCRFD/SCRFD /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOX/YOLOX /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOv34/YOLOv34 /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOv5_opt/YOLOv5_opt /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOv8_det/YOLOv8_det /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOv9_det/YOLOv9_det /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOv9_seg/YOLOv9_seg /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/segformer/segformer /mnt/sophonWP/sophon-demo/sample/tmp/
+
+rm -rf /mnt/sophonWP/sophon-demo/sample/P2PNet
+rm -rf /mnt/sophonWP/sophon-demo/sample/PP-OCR
+rm -rf /mnt/sophonWP/sophon-demo/sample/Real-ESRGAN
+rm -rf /mnt/sophonWP/sophon-demo/sample/SCRFD
+rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOX
+rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOv34
+rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOv5_opt
+rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOv8_det
+rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOv9_det
+rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOv9_seg
+rm -rf /mnt/sophonWP/sophon-demo/sample/segformer
+mv /mnt/sophonWP/sophon-demo/sample/tmp/P2PNet /mnt/sophonWP/sophon-demo/sample/
+mv /mnt/sophonWP/sophon-demo/sample/tmp/PP-OCR /mnt/sophonWP/sophon-demo/sample/
+mv /mnt/sophonWP/sophon-demo/sample/tmp/Real-ESRGAN /mnt/sophonWP/sophon-demo/sample/
+mv /mnt/sophonWP/sophon-demo/sample/tmp/SCRFD /mnt/sophonWP/sophon-demo/sample/
+mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOX /mnt/sophonWP/sophon-demo/sample/
+mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOv34 /mnt/sophonWP/sophon-demo/sample/
+mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOv5_opt /mnt/sophonWP/sophon-demo/sample/
+mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOv8_det /mnt/sophonWP/sophon-demo/sample/
+mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOv9_det /mnt/sophonWP/sophon-demo/sample/
+mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOv9_seg /mnt/sophonWP/sophon-demo/sample/
+mv /mnt/sophonWP/sophon-demo/sample/tmp/segformer /mnt/sophonWP/sophon-demo/sample/
+rm -rf /mnt/sophonWP/sophon-demo/sample/tmp
+
+
