14229c095c9b5c92e785debf3278960e7b8111b5
xuanhu001 <61825756+xuanhu001@users.noreply.github.com>
2024-08-09 15:51:02 +0800

    create some shell

---
+++ sample/scp_files.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# 注意会将文件传输至目标文件的子目录下！
+set -e
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/P2PNet linaro@IP:/mnt/sophonWP/sophon-demo/sample/P2PNet || exit 1
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/PP-OCR linaro@IP:/mnt/sophonWP/sophon-demo/sample/PP-OCR || exit 1
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/Real-ESRGAN linaro@IP:/mnt/sophonWP/sophon-demo/sample/Real-ESRGAN || exit 1
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/SCRFD linaro@IP:/mnt/sophonWP/sophon-demo/sample/SCRFD || exit 1
+# scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/StableDiffusionXL linaro@IP:/mnt/sophonWP/sophon-demo/sample/StableDiffusionXL || exit 1
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/YOLOX linaro@IP:/mnt/sophonWP/sophon-demo/sample/YOLOX || exit 1
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/YOLOv34 linaro@IP:/mnt/sophonWP/sophon-demo/sample/YOLOv34 || exit 1
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/YOLOv5_opt linaro@IP:/mnt/sophonWP/sophon-demo/sample/YOLOv5_opt || exit 1
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/YOLOv8_det linaro@IP:/mnt/sophonWP/sophon-demo/sample/YOLOv8_det || exit 1
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/YOLOv9_det linaro@IP:/mnt/sophonWP/sophon-demo/sample/YOLOv9_det || exit 1
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/YOLOv9_seg linaro@IP:/mnt/sophonWP/sophon-demo/sample/YOLOv9_seg || exit 1
+# scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/ppYOLOv3 linaro@IP:/mnt/sophonWP/sophon-demo/sample/ppYOLOv3 || exit 1
+scp -r /home/data/wcy/DEBUG/DLWP/sophon-demo/sample/segformer linaro@IP:/mnt/sophonWP/sophon-demo/sample/segformer || exit 1
+
+# mkdir /mnt/sophonWP/sophon-demo/sample/tmp
+# mv /mnt/sophonWP/sophon-demo/sample/P2PNet/P2PNet /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/PP-OCR/PP-OCR /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/Real-ESRGAN/Real-ESRGAN /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/SCRFD/SCRFD /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOX/YOLOX /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOv34/YOLOv34 /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOv5_opt/YOLOv5_opt /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOv8_det/YOLOv8_det /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOv9_det/YOLOv9_det /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/YOLOv9_seg/YOLOv9_seg /mnt/sophonWP/sophon-demo/sample/tmp/
+# mv /mnt/sophonWP/sophon-demo/sample/segformer/segformer /mnt/sophonWP/sophon-demo/sample/tmp/
+# rm -rf /mnt/sophonWP/sophon-demo/sample/P2PNet
+# rm -rf /mnt/sophonWP/sophon-demo/sample/PP-OCR
+# rm -rf /mnt/sophonWP/sophon-demo/sample/Real-ESRGAN
+# rm -rf /mnt/sophonWP/sophon-demo/sample/SCRFD
+# rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOX
+# rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOv34
+# rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOv5_opt
+# rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOv8_det
+# rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOv9_det
+# rm -rf /mnt/sophonWP/sophon-demo/sample/YOLOv9_seg
+# rm -rf /mnt/sophonWP/sophon-demo/sample/segformer
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/P2PNet /mnt/sophonWP/sophon-demo/sample/
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/PP-OCR /mnt/sophonWP/sophon-demo/sample/
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/Real-ESRGAN /mnt/sophonWP/sophon-demo/sample/
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/SCRFD /mnt/sophonWP/sophon-demo/sample/
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOX /mnt/sophonWP/sophon-demo/sample/
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOv34 /mnt/sophonWP/sophon-demo/sample/
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOv5_opt /mnt/sophonWP/sophon-demo/sample/
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOv8_det /mnt/sophonWP/sophon-demo/sample/
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOv9_det /mnt/sophonWP/sophon-demo/sample/
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/YOLOv9_seg /mnt/sophonWP/sophon-demo/sample/
+# mv /mnt/sophonWP/sophon-demo/sample/tmp/segformer /mnt/sophonWP/sophon-demo/sample/
+# rm -rf /mnt/sophonWP/sophon-demo/sample/tmp
+
+
---
+++ sample/show_model.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+# Get the list of directories in the current path and sort them by name
+directories=$(find . -maxdepth 1 -type d | LC_ALL=C sort)
+
+# Iterate over each directory
+for dir in $directories; do
+    # Check if the directory contains a "models" folder
+    if [ -d "$dir/models" ]; then
+        # Check if the "models" folder is empty
+        if [ "$(ls -A $dir/models)" ]; then
+            # Remove the specified directories from the "models" folder
+            rm -rf "$dir/models/BM1684X" "$dir/models/BM1688" "$dir/models/CV186X"
+
+            # Get the size of the "models" folder
+            size=$(du -sh "$dir/models" | awk '{print $1}')
+            # Print the directory name and the size of the "models" folder
+            echo -e "[${GREEN}INFO ${NC}] Directory: ${YELLOW}$dir${NC}, Models folder size: $size"
+        fi
+    else
+        # Print error message if "models" folder is not found
+        # echo -e "[${RED}ERROR${NC}] Directory: ${YELLOW}$dir${NC}, Models folder not found"
+        # 不输出
+        :
+    fi
+done
